53079fe197207614674b704a52694b311d3dc81e
Preston Hildebrandt <prestonhildebrandt@gmail.com>
2021-03-27 15:49:29 -0700

    navigation bar update v2

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -8,14 +8,19 @@ function Home() {
   return (
     <div className="Home">
       <header className="Home-header">
+      <l href="Steak N' Eggs" class="active">Steak N' Eggs</l> 
       <a href="home" class="active">Home</a>
       <a href="menu" class="active">Menu</a>
       <a href="order-online" class="active">Order-Online</a>
       <a href="rewards" class="active">Rewards</a>
       <a href="checkout" class="active">Checkout</a>
-      <a href="About" class="active">About</a>
+      <a href="about" class="active">About</a>
+      <a href="contact" class="active">Contact</a>
       </header>
     </div>
+
+    
+
   )
 }
 
