3ddc3c890227bf6c645c99e6ef7756cac9499bbd
Stephen Lam <stephenlam.me@gmail.com>
2021-04-05 16:41:33 -0700

    add (client):  implement simple router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import ReactDOM from 'react-dom'
 import './index.css'
 import Home from './views/Home'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Switch>
+        {/* <Route path="/menu"><Menu /></Route> */}
+        {/* <Route path="/rewards"><Rewards /></Route> */}
+        {/* <Route path="/checkout"><Checkout /></Route> */}
+        {/* <Route path="/contact"><Contact /></Route> */}
+        {/* <Route path="/about"><About /></Route> */}
+        <Route path="/"><Home /></Route>
+        {/* <Route><NotFound /></Route> */}
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 )
