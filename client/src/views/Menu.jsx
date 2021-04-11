d629f14d8cbe632c4c8ae2bc977dbe7f4d8ed049
Preston Hildebrandt <prestonhildebrandt@gmail.com>
2021-04-11 15:04:36 -0700

    menu.pdf added to menu tab

--- client/src/views/Menu.jsx
+++ client/src/views/Menu.jsx
@@ -1,18 +1,15 @@
 import React, { useState } from 'react'
-import file from '../assets/pdf/menu.pdf'
 import './Styles.css'
+
 
 function Menu() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-    <iframe
-            style={{ width: "563px", height: "666px" }}
-            src={file}
-            type='menu.pdf'
-            title='title'
-          />
+  <div>
+    <iframe class="iframe" src="https://drive.google.com/file/d/1_ekQGoISZvSaeCsDdqwDerejHdY4Y1O-/preview"
+     frameborder="0" height="1000" width="700">
+    </iframe>
   </div>
   )
   
