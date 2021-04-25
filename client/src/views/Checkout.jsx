24127f8c0910d0ef5cef4c105600d5db3e00b611
Preston Hildebrandt <prestonhildebrandt@gmail.com>
2021-04-25 16:21:13 -0700

    added quantity and totalprice functions

--- client/src/views/Checkout.jsx
+++ client/src/views/Checkout.jsx
@@ -9,28 +9,46 @@ function Checkout() {
   
 console.log(checkoutList)
 
-// const handleRemove = (item) => {
-//   setCheckoutItems(old => [...old, item]) 
-//   localStorage.setItem("checkoutItems", JSON.stringify(checkoutItems));
-//   alert(`${item.item_title} added to chekout`)
-// }
+
+
+// button to remove items from cart
+const handleRemove = (item) => {
+setCheckoutItems(old => [...old, item]) 
+localStorage.setItem("checkoutItems", JSON.stringify(checkoutItems));
+alert(`${item.item_title} added to chekout`)
+}
+
+
+// function to calculate totalprice in cart 
+const totalPrice = (checkoutList, item_price) => {
+  
+}
+
+
+// another function for checkout (checkout should wipe entire cart)
 
 if(checkoutList.length < 1){
   return <h2>No checkouts available</h2>
 }
 else{
   return (
-  <div>
-    <h2>Checkout Page</h2>
+  <div className="cart-header">
+  <section className="cart-style">
+    <h1>Your Cart:</h1>
     {
       checkoutList.map(item => (
         <div>
-              <p>Title: {item.item_title}</p>
+              <p>{item.item_title}</p>
               <p>{item.item_price}</p>           
-              <button onClick={() => handleRemove(item)}>Remove from cart</button>
-          </div>
+              <button onClick={() => handleRemove(item)}>Remove</button>
+        </div>
       ))
     }
+  </section>
+  <section className="cart-header-border2">
+    <p>Total Items in Cart: {checkoutList.length}</p>
+    <p>Subtotal: {checkoutList.item_price} </p>
+  </section>
   </div>
   )
 }
