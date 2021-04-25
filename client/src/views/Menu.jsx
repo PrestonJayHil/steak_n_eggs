a1fea9fe4d5b46a1ac2a788b05a19d8cd2fc908e
Preston Hildebrandt <prestonhildebrandt@gmail.com>
2021-04-25 16:58:38 -0700

    added downloadable menu feature

--- client/src/views/Menu.jsx
+++ client/src/views/Menu.jsx
@@ -9,8 +9,12 @@ import menu from '../assets/jpeg/menu.jpg'
 function Menu() {
   const [count, setCount] = useState(0)
   return (
-    <div>
-      <img src={menu} alt="" width="950px" height="1400px" class="center"/>
+    <div className="center">
+      <h2>Click Menu to Download</h2>
+      <a href={menu} target="_blank" download={menu} width="950px" height="1400px">
+      <img src={menu} alt="" width="950px" height="1400px"/>
+      </a>
+      
     </div>
   )
 }
