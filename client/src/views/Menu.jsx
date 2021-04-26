fcc9dea5dc8312e49ebda54a689c591f3a2ac0ad
Preston Hildebrandt <prestonhildebrandt@gmail.com>
2021-04-25 17:07:00 -0700

    fixed title for when user downloads .jpg

--- client/src/views/Menu.jsx
+++ client/src/views/Menu.jsx
@@ -1,20 +1,15 @@
 import React, { useState } from 'react'
 import './Styles.css'
-import menu_eggs from '../assets/jpeg/menu_eggs.jpg'
-import menu_steak from '../assets/jpeg/menu_steak.jpg'
 import menu from '../assets/jpeg/menu.jpg'
-
-
 
 function Menu() {
   const [count, setCount] = useState(0)
   return (
     <div className="center">
       <h2>Click Menu to Download</h2>
-      <a href={menu} target="_blank" download={menu} width="950px" height="1400px">
+      <a href={menu} target="_blank" download="steak_n_eggs_menu" width="950px" height="1400px">
       <img src={menu} alt="" width="950px" height="1400px"/>
       </a>
-      
     </div>
   )
 }
