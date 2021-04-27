d4ffbdf7276b07f7737bb11990f7bc1cf101b69d
Adara <73359746+Princ3ssAdara@users.noreply.github.com>
2021-04-26 19:47:16 -0700

    Formatting update
    Added a brief intro and photo
    Need to add more to fill up the page, possibly more images
    Maybe a small rating system at the bottom?

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -3,15 +3,19 @@ import React, { useState } from 'react'
 import './Styles.css'
 
 function Home() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-  <div>
-    <h2>Home Page</h2>
-  </div>
-  )
+    return (
+      <div>
+        <h1>Welcome to Steak N' Eggs</h1>
+        <p>Steak N' Eggs restaurant is an all-day diner offering delicious breakfast, lunch, and dinner options
+            along with our famous house special, Steak-N-Eggs. <br></br> We welcome you to join us for a fun and inviting 
+            dining-in experience or to take advantage of our convenient to-go options giving you the flexibility
+            to eat at home, at work, or in one of our breakfast booths!</p>
+        <iframe class="iframe" src="https://drive.google.com/file/d/1ASw-2cwNT-fOnb_cZHWlOrWrrUSuyKC_/view"
+            frameborder="0"> </iframe>
+      </div>
+    );
 }
 
 export default Home
-
-
