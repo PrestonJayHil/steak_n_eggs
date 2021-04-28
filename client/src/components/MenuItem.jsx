55818cee2a7d42074415c05a505c608f7681bfba
Stephen Lam <stephenlam.me@gmail.com>
2021-04-28 14:14:36 -0700

    update (client): add image to MenuItem and move images to public folder

--- client/src/components/MenuItem.jsx
+++ client/src/components/MenuItem.jsx
@@ -1,35 +1,14 @@
 import React, {useState} from 'react'
-import FrenchToastCombo from '../assets/menu_assets/french_toast_combo.jpg'
-import SteakNEggs from '../assets/menu_assets/steak_n_eggs.jpg'
-
-
 
 export default function MenuItem({ item }) {
-
-
-    return (
-        <div>
-         <h2>{item.item_title} {item.item_price} </h2>
-        <div> </div>
-         <h3>{item.item_desc}</h3>
-        </div>
-
-    )
+  const imgPath = `${item.item_title
+    .replaceAll(/\W/g, '')
+    .toLowerCase()}.jpg`;
+  return (
+      <div>
+        <img src={imgPath} />
+        <h2>{item.item_title} {item.item_price}</h2>
+        <p>{item.item_desc}</p>
+      </div>
+  )
 }
-
-
-
-
-    
-  /*  items.forEach((item) => {
-        filepath = name_normalize_function(item.item_title);
-        return (<img src={filepath} />);
-      })
-
-        const firstItem = items[0]
-        const filepath = name_normalize_function(firstItem.item_title);
-        return (<img src={filepath} />);
-
-    */
-
-    
