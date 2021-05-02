5c4666dbc805a4c34ad6b492f08b31389a0162fa
Stephen Lam <stephenlam.me@gmail.com>
2021-05-01 20:11:11 -0700

    add (frontend): login capability

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import React, { useEffect, useState } from 'react'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import ReactDOM from 'react-dom'
+import { Auth0Provider, useAuth0 } from '@auth0/auth0-react';
+
 import './index.css'
 import Home from './views/Home'
 import Menu from './views/Menu'
@@ -10,37 +12,79 @@ import Checkout from './views/Checkout'
 import Contact from './views/Contact'
 import About from './views/About'
 
-ReactDOM.render(
-  <React.StrictMode>
+function Nav() {
+  const {
+    loginWithRedirect,
+    isAuthenticated,
+    logout,
+  } = useAuth0();
+
+  return (
+    <header className="Home-header">
+      <nav className="Home-nav">
+        <Link className="Nav-link" to="/">Steak N' Eggs</Link>
+        <Link className="Nav-link" to="home">Home</Link>
+        <Link className="Nav-link" to="menu">Menu</Link>
+        <Link className="Nav-link" to="order">Order-online</Link>
+        <Link className="Nav-link" to="rewards">Rewards</Link>
+        <Link className="Nav-link" to="checkout">Checkout</Link>
+        <Link className="Nav-link" to="about">About</Link>
+        <Link className="Nav-link" to="contact">Contact</Link>
+      </nav>
+      {
+        isAuthenticated ?
+        <button
+          className="login-btn"
+          onClick={logout}
+        >
+          Log Out
+        </button>
+        :
+        <button
+          className="login-btn"
+          onClick={loginWithRedirect}
+        >
+          Log In
+        </button>
+      }
+
+    </header>
+  );
+}
+
+function App() {
+  return (
     <BrowserRouter>
-    <div className="Home">
-      <header className="Home-header">
-      <l href="Steak N' Eggs" class="active">Steak N' Eggs</l> 
-      <a href="home" class="active">Home</a>
-      <a href="menu" class="active">Menu</a>
-      <a href="order" class="active">Order-online</a>
-      <a href="rewards" class="active">Rewards</a>
-      <a href="checkout" class="active">Checkout</a>
-      <a href="about" class="active">About</a>
-      <a href="contact" class="active">Contact</a>
-      </header>
-      <Switch>
-        {<Route path="/menu"><Menu /></Route>}
-        {<Route path="/order"><Order /></Route>}
-        {<Route path="/rewards"><Rewards /></Route>}
-        {<Route path="/checkout"><Checkout /></Route>}
-        {<Route path="/contact"><Contact /></Route>}
-        {<Route path="/about"><About /></Route>}
-        <Route path="/"><Home /></Route>
-        {/* <Route><NotFound /></Route> */}
-      </Switch>
+      <div className="Home">
+        <Auth0Provider
+          domain={import.meta.env.VITE_AUTH0_DOMAIN}
+          clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+          redirectUri={window.location.origin}
+        >
+          <Nav />
+          <Switch>
+            <Route path="/menu"><Menu /></Route>
+            <Route path="/order"><Order /></Route>
+            <Route path="/rewards"><Rewards /></Route>
+            <Route path="/checkout"><Checkout /></Route>
+            <Route path="/contact"><Contact /></Route>
+            <Route path="/about"><About /></Route>
+            <Route path="/"><Home /></Route>
+            {/* <Route><NotFound /></Route> */}
+          </Switch>
+        </Auth0Provider>
       </div>
 
-    <div className="Body">
-      <h2>Welcome to our restaurant!</h2>
-      </div> 
-
+      <div className="Body">
+        <h2>Welcome to our restaurant!</h2>
+      </div>
     </BrowserRouter>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById('root'),
+);
