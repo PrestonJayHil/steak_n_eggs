c9b3b06d35b7160a7cf544997994298b64030b79
Adara <73359746+Princ3ssAdara@users.noreply.github.com>
2021-05-18 02:20:00 -0700

    Update About.jsx
    Updated about page
    Used previous style formatting 
    Reformatted title
    Reformatted body
    Added photo

--- client/src/views/About.jsx
+++ client/src/views/About.jsx
@@ -1,23 +1,35 @@
 import React, { useState } from 'react'
 // import logo from '../assets/svg/logo.svg'
 import './Styles.css'
+import about from '../assets/jpeg/about.jpg'
 
 function About() {
   const [count, setCount] = useState(0)
 
   return (
   <div>
-    <h2>About</h2>
-    <p style={{textAlign: 'left'}}>Steak N' Eggs was founded in 2021 by hungry college students Stephen, Preston, Noah, and Adara. <br></br>
+    <h1 style={{textAlign: 'center'}}> About </h1>
+    <p style={{textAlign: 'left', fontSize:'20px', paddingLeft: '220px'}}>Steak N' Eggs was founded in 2021 by hungry college students Stephen, Preston, Noah, and Adara. <br></br>
         Since most mornings began with zoom classes, there was not always enough time to get a healthy and fulfilling breakfast.<br></br>
         Seeking to make a change, the four began planning, and Steak N' Eggs was born.<br></br>
-        The idea that a takeaway restaurant could increase health, happiness, and alertness seemed crazy at first, but studies
-        have shown that less than 44% of Americans eat breakfast despite 93% saying it was the most important meal of the day.<br></br>
-        Steak N' Eggs seeks to offer a quick and convenient breakfast, lunch, or dinner option for everyone and has since expanded into
-        the traditional dine-in restaurant style to enhance customer experience.<br></br>
+        The idea that a takeaway restaurant could increase health, happiness, and alertness seemed crazy at first, <br>
+        </br>but studies have shown that less than 44% of Americans eat breakfast, 
+        <br></br> despite 93% saying it was the most important meal of the day.<br></br>
+        Steak N' Eggs seeks to offer a quick and convenient breakfast, lunch, or dinner option for everyone 
+        <br></br>and has since expanded into the traditional dine-in restaurant style to enhance customer experience.<br></br>
         </p>
+      <div>
+        <center>
+          <img src={about} 
+          alt="photo of steak and eggs" 
+          width="500" 
+          height="300"
+          textAlign= "center"
+           />
+        </center>
+      </div>
   </div>
-  )
+  );
 }
 
 export default About
