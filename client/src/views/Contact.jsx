5575f716eb6196c428a9c3db753442ce7c7d7d00
Adara <73359746+Princ3ssAdara@users.noreply.github.com>
2021-05-18 01:49:25 -0700

    Update Contact.jsx
    Change in formatting to match other pages
    Added a jpg photo

--- client/src/views/Contact.jsx
+++ client/src/views/Contact.jsx
@@ -1,23 +1,35 @@
 import React, { useState } from 'react'
 // import logo from '../assets/svg/logo.svg'
 import './Styles.css'
+import contact from '../assets/jpeg/contact.jpg'
 
 function Contact() {
   const [count, setCount] = useState(0)
 
-  return (
-  <div>
-    <h2>Contact Us</h2>
-    <p>Did you enjoy your visit? Tell your friends about us! <br></br>
-    If you didn't enjoy your visit... tell your enemies! <br></br>
-       <br></br>
-       <br></br>
-       We'd also love to hear from you about your experience. Please leave us a review online or send us an email.
-       <br></br>
-       Restaurant owner: Stephen <br></br>
-       General Managers: Preston, Noah, and Adara <br></br> </p>
-  </div>
-  )
+ return (
+  <div class="flex-container">
+      <div class="flex-child">
+        <div>
+          <img src={contact} alt=""/>
+        </div>
+      </div>
+
+    <div class="flex-child">
+      <div className="contact-context-1">
+        <p> Contact Us</p>
+      </div>
+      <div className = "contact-context-2">
+      <p> Did you enjoy your visit? Tell your friends about us! </p>
+      <p> If you didn't enjoy your visit... tell your enemies! </p>
+      <p> We'd also love to hear from you about your experience. </p>
+      <p> Please leave us a review online or send an email to one of us</p>
+      <p> </p>
+      <p> Restaurant owner: Stephen </p>
+      <p> General Managers: Preston, Noah, and Adara </p>
+      </div>
+    </div>
+    </div>
+    );
 }
 
 export default Contact
