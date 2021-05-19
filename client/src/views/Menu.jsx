b0d09c08b932fc83e20862f5be2c2d610056fc59
Preston Hildebrandt <prestonhildebrandt@gmail.com>
2021-05-18 20:04:53 -0700

    fixed color in menu tab so user can see header message

--- client/src/views/Menu.jsx
+++ client/src/views/Menu.jsx
@@ -6,7 +6,7 @@ function Menu() {
   const [count, setCount] = useState(0)
   return (
     <div className="center">
-      <h2>Click Menu to Download</h2>
+      <h2>Click on Menu to Download</h2>
       <a href={menu} target="_blank" download="steak_n_eggs_menu" width="950px" height="1400px">
       <img src={menu} alt="" width="950px" height="1400px"/>
       </a>
